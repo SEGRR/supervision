b96ce609561b0222f2d97eb381ebd4a4b52c64de
segrr <shaikhsufiyan2003@gmail.com>
2024-04-15 22:26:35 +0530

    Changes to seating schema

--- models/seatingArrangement.js
+++ models/seatingArrangement.js
@@ -1,26 +1,18 @@
 const mongoose = require("mongoose");
 
-
 const seatingArrangementSchema = new mongoose.Schema({
    divisionsPerYear:{
-      // type:[
-      //    {
-            // className:String,
-            // startRollNo:Number,
-            // endRollNo:Number,
-            // total:Number,
-            // department:String
-      //    }
-      // ],
       type:Map,
       required:true
    },
-   examSlotsPerDay:Number,
+   examSlotsPerDay:String,
    examStartDate:Date,
    examTimeSlots:[
       { startTime: String, endTime: String }
    ],
-   examdays:Number,
+   examdays:String,
+   examStartDate:String,
+   examDates:Map,
    selectedAcademicYear:String,
    selectedClassrooms:[
       { name:String, capacity: Number }
